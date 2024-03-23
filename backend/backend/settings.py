2c922bf485761e3ed3bac24887d6957d426313f1
Alisher <lojkaandvilka@yandex.ru>
2024-03-24 00:41:53 +0500

    lll

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,6 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-
 ALLOWED_HOSTS = ['51.250.96.255', '127.0.0.1', 'localhost', 'nopetask.hopto.org']
 
 INSTALLED_APPS = [
